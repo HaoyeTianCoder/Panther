f5d6115313b892bd38f32387efa2865ce5876b00
haoye tian <363730631@qq.com>
2021-04-08 14:52:09 +0200

    none

--- config_default.py
+++ config_default.py
@@ -8,8 +8,9 @@ import json
 class Config:
     def __init__(self):
         self.defects4j_buggy = '/Users/haoye.tian/Documents/University/project/defects4j_buggy'
-        self.path_dataset = '/Users/haoye.tian/Documents/University/data/PatchCollectingTOSEMUnique'
-        self.version = 'Cross4'
+        self.path_dataset = '/Users/haoye.tian/Documents/University/data/PatchCollectingTOSEMYeUnique'
+        # self.path_dataset = '/Users/haoye.tian/Documents/University/data/PatchSimTOSEM'
+        self.version = 'Cross_bigdata'
         self.wcv = 'Bert'
         self.learned_feature = ''
         self.engineered_feature = ''
