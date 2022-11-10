e2482d0781d223b872f64ca1ca4722ebd882fcae
haoyetiancoder <363730631@qq.com>
2022-11-10 22:55:13 +0100

    rename

--- config_default.py
+++ config_default.py
@@ -9,7 +9,7 @@ class Config:
     def __init__(self):
         self.defects4j_buggy = '/Users/haoye.tian/Documents/University/project/defects4j_buggy'
         self.path_dataset = '/Users/haoye.tian/Documents/University/data/PatchCollectingTOSEMYeUnique'
-        # self.path_dataset = '/Users/haoye.tian/Documents/University/data/PatchSimTOSEM'
+        self.path_testdata = '/Users/haoye.tian/Documents/University/data/PatchSimTOSEM'
         self.version = 'Cross_bigdata'
         self.wcv = 'Bert'
         self.learned_feature = ''
